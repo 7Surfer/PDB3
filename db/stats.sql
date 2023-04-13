b028dda618dd127e42f9d87f97f3ab572e64c4ab
Luca Heinrich <Heinrich.Luca@gmx.net>
2023-04-13 23:07:29 +0200

    update notfy and stats create statements

--- db/stats.sql
+++ db/stats.sql
@@ -24,12 +24,24 @@ CREATE TABLE IF NOT EXISTS public.stats
     "unitsLost" bigint,
     "playerId" integer NOT NULL,
     "timestamp" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    "realDebrisMetal" bigint,
+    "realDebrisCrystal" bigint,
+    "realUnitsDestroyed" bigint,
+    "realUnitsLost" bigint,
     CONSTRAINT stats_pkey PRIMARY KEY ("dbKey")
 )
-WITH (
-    OIDS = FALSE
-)
+
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.stats
     OWNER to poll;
+
+GRANT TRUNCATE, TRIGGER ON TABLE public.stats TO "pdb-admin";
+
+GRANT DELETE ON TABLE public.stats TO "pdb-overquota";
+
+GRANT SELECT ON TABLE public.stats TO "pdb-ro";
+
+GRANT UPDATE, DELETE, INSERT, REFERENCES ON TABLE public.stats TO "pdb-rw";
+
+GRANT ALL ON TABLE public.stats TO poll;
